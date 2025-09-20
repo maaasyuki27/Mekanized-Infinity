607670ad1c37fe8f7a04a61dda66ee4318c8d7e3
maaasyuki27 <maaasyuki27@gmail.com>
2025-09-20 10:54:43 +0900

    Update item.js

--- kubejs/startup_scripts/item.js
+++ kubejs/startup_scripts/item.js
@@ -1,5 +1,10 @@
 StartupEvents.registry('item', event => {
-  event.create('mek_inf:mainlogo').texture('kubejs:item/mek')
-  event.create('mek_inf:sugar_cube').texture('kubejs:item/sugar_cube')
-  event.create('mek_inf:bio_cube').texture('kubejs:item/bio_cube')
+    event.create('mek_inf:mainlogo').texture('kubejs:item/mek')
+    event.create('mek_inf:sugar_cube').texture('kubejs:item/sugar_cube')
+    event.create('mek_inf:bio_cube').texture('kubejs:item/bio_cube')
+
+    let types = ["simple","basic","advanced","elite","ultimate","overclocked","quantum","dense","multiversal","creative"]
+    types.forEach(type => {
+      event.create(`mek_inf:${type}_resister`).texture(`kubejs:item/${type}_resister`)
+    })
 })
